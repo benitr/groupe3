d1aedfaf9f97451671a512e6e5354f3583326e2e
bill89 <romain.benit@laposte.net>
2019-01-03 16:10:28 -0500

    Ajout d'une fontion dictionnaire pour ranger la colonne dns_ns

--- script.py
+++ script.py
@@ -4,18 +4,22 @@
 #########################
 #	Script python	#
 #	BENIT Romain	#
-#	Version 1	#
+#	Version 1.1	#
 #########################
 
 # Import des modules
 import csv, sys
 
 # Initialisation des variables
-dns_a_origin = "toto"
-dns_ns_origin = "toto"
-cpt_dns_a = 0
-cpt_dns_ns = 0
-dico = {}
+dico_dns_ns = {}
+
+# Initialisation des fonctions
+def dictionnaire(dico, colonne):
+    if dico.has_key(colonne):
+        dico[colonne] = int(dico.get(colonne))+1
+    else :
+        dico[colonne] = 1
+    return dico
 
 # Debut du script
  
@@ -35,18 +39,10 @@ with open(filename) as f:
             geoip_country = row[6]
             ssdeep_score = row[7]
 
-            if fuzzer == "Original*":
-                dns_a_origin = dns_a
-                dns_ns_origin = dns_ns
-            
-            cpt_dns_a = cpt_dns_a + dns_a.count(dns_a_origin)
-            cpt_dns_ns = cpt_dns_ns + dns_ns.count(dns_ns_origin)
+            # Utilisation des disctionnaires pour les listes finales
+	    dictionnaire(dico_dns_ns, dns_ns)
 
-	    dico[dns_ns] = cpt_dns_ns
     except csv.Error as e:
         sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 
-print "Il y a ",cpt_dns_a," fois l'entre ",dns_a_origin," dans le fichier"
-print "Il y a ",cpt_dns_ns," fois l'entre ",dns_ns_origin," dans le fichier"
-
-print dico
+print dictionnaire(dico_dns_ns, dns_ns)
